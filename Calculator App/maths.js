dc690b806c71913a2100961edbc320c214ad52b8
Alejandro Hernandez <AlejandroHernandez@k2view.com>
2022-02-24 23:27:12 -0600

    Added packageless nodeJS calculator

---
+++ Calculator App/maths.js
@@ -0,0 +1,19 @@
+exports.sum = function (a,b) {
+    return (a + b);
+}
+
+exports.subtract = function (a,b) {
+    return (a - b);
+}
+
+exports.multiply = function (a,b) {
+return (a * b);
+}
+
+exports.divide = function (a,b) {
+    return (a / b);
+}
+
+exports.module = function (a,b) {
+    return (a % b);
+}
